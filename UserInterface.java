1ed921c8bdb025001a42730c7cb522bc21d8b524
muhamedcakaj <muhamedcakaj57@gmail.com>
2023-04-30 19:43:26 +0200

    New

---
+++ UserInterface.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class UserInterface{
+    private Scanner scan;
+    
+    public UserInterface(Scanner scan){
+        this.scan=scan;
+    }
+    public void start(){
+        
+    }
+}
